f614bba7fdd0215bf0f42332af7718ad377b61cd
teleportsing <teleportsing@gmail.com>
2023-07-19 13:15:01 +0800

    add router-link

--- week1/index.jsx
+++ week1/index.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 function App() {
   return (
-    <h1>Hello React Router!</h1>
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="about">About</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+function Home() {
+  return (
+    <h1>hello, React Router!</h1>
+  )
+}
+function About() {
+  return (
+    <h1>About page goes here!</h1>
+  )
+}
+
+ReactDOM.
+  createRoot(document.getElementById('root'))
+  .render(<App />);
