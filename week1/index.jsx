e272b26b0a18e04e1b2ee160b0edfc495d358096
teleportsing <teleportsing@gmail.com>
2023-07-23 12:41:21 +0800

    add server.js

--- week1/index.jsx
+++ week1/index.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+
+import Home from "./pages/Home"
+import About from "./pages/About"
+
+import "./server"
+
 function App() {
   return (
     <BrowserRouter>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="about">About</Link>
-      </nav>
+      <header>
+        <Link to="/">#vanlife</Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -16,16 +24,16 @@ function App() {
   )
 }
 
-function Home() {
-  return (
-    <h1>hello, React Router!</h1>
-  )
-}
-function About() {
-  return (
-    <h1>About page goes here!</h1>
-  )
-}
+// function Home() {
+//   return (
+//     <h1>hello, React Router!</h1>
+//   )
+// }
+// function About() {
+//   return (
+//     <h1>About page goes here!</h1>
+//   )
+// }
 
 ReactDOM.
   createRoot(document.getElementById('root'))
